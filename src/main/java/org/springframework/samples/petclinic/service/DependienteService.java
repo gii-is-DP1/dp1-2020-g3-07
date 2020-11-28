83969d9f29e8e01dd6c091c662b1c91ac19db05b
Daniel Rico Ostos <72860383+danricost@users.noreply.github.com>
2020-11-28 16:24:04 +0100

    dependiente

--- src/main/java/org/springframework/samples/petclinic/service/DependienteService.java
+++ src/main/java/org/springframework/samples/petclinic/service/DependienteService.java
@@ -1,8 +1,13 @@
 package org.springframework.samples.petclinic.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.repository.DependienteRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DependienteService {
 
+	@Autowired
+	DependienteRepository dependienteRepos;
+	
 }
