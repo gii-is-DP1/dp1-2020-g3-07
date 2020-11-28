4db07733d40ae552229c63fb2c2c2e721e032615
Rodrigosg00 <rodrigo26stj@gmail.com>
2020-11-28 21:02:11 +0100

    c

--- src/main/java/org/springframework/samples/petclinic/service/PedidoService.java
+++ src/main/java/org/springframework/samples/petclinic/service/PedidoService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.Pedido;
-import org.springframework.samples.petclinic.model.Producto;
 import org.springframework.samples.petclinic.repository.PedidoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
